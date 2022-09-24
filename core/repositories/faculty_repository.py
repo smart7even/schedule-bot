8045d751227ac76c61237e4dd717e238359f68bc
smart7even <smart7even@yandex.ru>
2022-09-24 22:27:26 +0300

    feat: add web api with faculty and group handlers

--- core/repositories/faculty_repository.py
+++ core/repositories/faculty_repository.py
@@ -1,12 +1,19 @@
-from typing import List
+from typing import List, Optional
+
+from sqlalchemy.orm import joinedload
 
 from core.models.faculty import Faculty
+from core.models.group import Group
 from db import Session
 
 
 class FacultyRepository:
     def __init__(self, session: Session) -> None:
         self.session = session
+
+    def get_by_id(self, faculty_id: int) -> Optional[Faculty]:
+        """Gets faculty by id"""
+        return self.session.query(Faculty).options(joinedload(Faculty.groups)).filter(Group.faculty_id == faculty_id).first()
 
     def get_all(self) -> List[Faculty]:
         """Gets all faculties stored in db"""
---
+++ server.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+
+from core.models import group
+from core.models import faculty
+from core.repositories.faculty_repository import FacultyRepository
+from core.repositories.group_repository import GroupRepository
+from db import Session
+
+app = FastAPI()
+
+
+@app.get("/faculty")
+async def get_faculties():
+    session = Session()
+    faculties_repository = FacultyRepository(session)
+    faculties = faculties_repository.get_all()
+
+    return {"faculties": faculties}
+
+
+@app.get("/faculty/{faculty_id}")
+async def get_faculty_by_id(faculty_id: int):
+    session = Session()
+    faculties_repository = FacultyRepository(session)
+    faculty = faculties_repository.get_by_id(faculty_id)
+
+    return faculty
+
+
+@app.get("/group")
+async def get_groups():
+    session = Session()
+    group_repository = GroupRepository(session)
+    groups = group_repository.get_all()
+
+    return {"groups": groups}
+
+
+@app.get("/group/{group_id}")
+async def get_group_by_id(group_id: int):
+    session = Session()
+    group_repository = GroupRepository(session)
+    group = group_repository.get_group_by_id(group_id)
+
+    return group
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
